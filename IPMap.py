689443bd2eb57cf592ac8ace1fec100ef26ef2a1
RishabhBudhia <59286478+RishabhBudhia@users.noreply.github.com>
2022-04-23 22:04:59 +0530

    Add files via upload

--- IPMap.py
+++ IPMap.py
@@ -1,12 +1,14 @@
 import geocoder
 import folium
+from pprint import pprint
 
 ## public ip
 g = geocoder.ip("me")
+pprint(g.geojson)
 myAddress = g.latlng
 
 myMap1 = folium.Map(location=myAddress,zoom_start=12)
 folium.CircleMarker(location=myAddress,radius=50,popup="India").add_to(myMap1)
 folium.Marker(myAddress,popup="Vellore").add_to(myMap1)
 myMap1.save("MapIPOutput.html")
-print(myAddress)
+pprint(myAddress)
